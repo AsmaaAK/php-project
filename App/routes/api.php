50c73f5e7efd9dcbc1bca2e6c573c55f944dec03
AsmaaAK <asoomak97@gmail.com>
2025-08-25 06:31:17 -0700

    add routers to api file

--- App/routes/api.php
+++ App/routes/api.php
@@ -3,16 +3,25 @@ use App\Core\RouterAPI;
 use App\Controllers\AuthController;
 use App\Controllers\UserController;
 use App\Controllers\VolunteerController;
+use App\Controllers\EventController;
 
 $routerApi = new RouterAPI();
 $routerApi->post('/capstone4-mvc/public/api/login', [AuthController::class, 'apiLogin'], false);
 $routerApi->post('/capstone4-mvc/public/api/register', [AuthController::class, 'apiRegister'], true);
 $routerApi->get('/capstone4-mvc/public/api/users', [UserController::class, 'getAllUsers'], true);
+
 $routerApi->get('/capstone4-mvc/public/api/volunteers', [VolunteerController::class, 'index']);
 $routerApi->get('/capstone4-mvc/public/api/volunteers/{id}', [VolunteerController::class, 'show']);
 $routerApi->post('/capstone4-mvc/public/api/volunteers', [VolunteerController::class, 'store'], true);
 $routerApi->put('/capstone4-mvc/public/api/volunteers/{id}', [VolunteerController::class, 'update'], true);
 $routerApi->delete('/capstone4-mvc/public/api/volunteers/{id}', [VolunteerController::class, 'delete'], true);
+
+$routerApi->get('/capstone4-mvc/public/api/events', [EventController::class, 'index']);
+$routerApi->get('/capstone4-mvc/public/api/events/{id}', [EventController::class, 'show']);
+$routerApi->post('/capstone4-mvc/public/api/events', [EventController::class, 'store'], true);
+$routerApi->put('/capstone4-mvc/public/api/events/{id}', [EventController::class, 'update'], true);
+$routerApi->delete('/capstone4-mvc/public/api/events/{id}', [EventController::class, 'delete'], true);
+
 
 
 $routerApi->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
